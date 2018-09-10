8653f2c36e6a70e7086819c53434ae3ae724f0b6
Alisher Radjabov <advaster@yahoo.com>
2018-09-10 14:31:30 +0400

    BlogBundle

---
+++ src/BlogBundle/Controller/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace BlogBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+    /**
+     * @Route("/user", name="user")
+     */
+    public function indexAction(Request $request)
+    {
+        return new Response('User controller');
+    }
+}
